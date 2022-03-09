01678d1380228dc995631a0d74255530ec6a99d7
sean-ttm <sangwon.cfa@gmail.com>
2022-03-09 14:36:16 +0900

    Add proof of work concept

--- blockchain/block.go
+++ blockchain/block.go
@@ -4,22 +4,31 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/sean-ttm/sbtcoin/db"
 	"github.com/sean-ttm/sbtcoin/utils"
 )
 
+const difficulty int = 2
+
 type Block struct {
-	Data 	 string `json:"data"`
-	Hash	 string `json:"hash"`
-	PrevHash string `json:"prevHash,omitempty"`
-	Height	 int 	`json:"height"`
+	Data 	 	string  `json:"data"`
+	Hash	 	string  `json:"hash"`
+	PrevHash 	string  `json:"prevHash,omitempty"`
+	Height	 	int 	`json:"height"`
+	//Added for Proof of Work
+	Difficulty 	int		`json:difficulty`
+	//Nonce is the only thing that can be chaned by miners
+	Nonce		int		`json:nonce`
 }
 
+//db에 block 저장
 func (b *Block) persist(){
 	db.SaveBlock(b.Hash, utils.ToBytes(b))
 }
 
+//define not found error 
 var ErrNotFound = errors.New("block not found")
 
 func (b *Block) restore(data []byte){
@@ -36,15 +45,38 @@ func FindBlock(hash string) (*Block, error){
 	return block, nil
 }
 
+func (b *Block) mine(){
+	//0이 difficulty 만큼 연속으로 있는 Hash 찾기 위함
+	target := strings.Repeat("0", b.Difficulty)
+	for {
+		//Block string으로 바꿈
+		blockAsString := fmt.Sprint(b)
+		//sha256 이용해서 Hash 생성
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
+		fmt.Printf("Block as String:%s\nHash:%s\nNonce:%d\n\n\n", blockAsString, hash, b.Nonce )
+		//string.Hasprefix -> hash가 target="00"으로 시작하는지 return하는 함수
+		if strings.HasPrefix(hash, target){
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
 func creatBlock(data string, prevHash string, height int) *Block{
 	block := &Block{
 		Data: data,
 		Hash: "",
 		PrevHash: prevHash,
 		Height: height,
+		//const difficulty predefined
+		Difficulty: difficulty,
+		Nonce: 0,
 	}
-	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	//mining function - proof of work
+	block.mine()
+	//save block data to DB
 	block.persist()
 	return block
 }
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"github.com/sean-ttm/sbtcoin/cli"
-	"github.com/sean-ttm/sbtcoin/db"
-)
+import "github.com/sean-ttm/sbtcoin/cli"
 
 func main() {	
 	//defer executes when the main closed
-	defer db.Close()
+	//defer db.Close()
 	cli.Start()
 }
+
